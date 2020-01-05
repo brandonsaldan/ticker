8452fdd4cd3d08c8041123d22884c42aa57d8658
brandonsaldan <26472557+shibbbe@users.noreply.github.com>
2020-01-05 00:19:42 -0500

    Create stockprice.py

---
+++ cogs/stockprice.py
@@ -0,0 +1,39 @@
+import discord
+import asyncio
+import math
+import datetime
+import requests
+import json
+import string
+import time
+import os
+import os.path
+import traceback
+
+from discord.ext import commands
+from discord.ext.commands import Bot
+from datetime import datetime, date
+from random import randint
+from io import StringIO
+from os import listdir
+from os.path import isfile, join
+
+class StockPrice(object):
+	def __init__(self, bot):
+		self.bot = bot
+	@commands.command(pass_context=True)
+	async def stockprice(self, ctx, *, arg):
+		print("stockprice command recieved.")
+		print("--------------------")
+		embed = discord.Embed(title=arg, color = 0x21CE99)
+		embed.set_footer(text="{} | Financial Modeling Prep API ".format(arg), icon_url="https://upload.wikimedia.org/wikipedia/commons/b/b9/Robinhood_Logo.png")
+		
+		symbol = requests.get("https://financialmodelingprep.com/api/v3/stock/real-time-price/{}".format(arg)).json()
+		price = requests.get("https://financialmodelingprep.com/api/v3/stock/real-time-price/{}".format(arg)).json()
+		
+		embed.add_field(name="Symbol", value="{}".format(symbol.get("symbol", "Not found")))
+		embed.add_field(name="Price", value="${}".format(price.get("price", "Not found")))
+		await self.bot.say(embed=embed)
+
+def setup(bot):
+	bot.add_cog(StockPrice(bot))
